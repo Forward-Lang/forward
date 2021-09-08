50214a65c339af07c874c28304a728f9173f6c89
Bruno-366 <81762173+Bruno-366@users.noreply.github.com>
2021-09-08 22:01:36 +0200

    Update others-mainstream.sh

--- why/others-mainstream.sh
+++ why/others-mainstream.sh
@@ -1,32 +1,4 @@
-# = Programming Paradigms: and their constructs 
-#
-# The three main programming paradigms in widespread use today are:
-#
-# . Imperative
-# . Functional
-# . Object oriented 
-#
-# Each paradigm has different a different approach to how they solve problems.
-#
-# .Here's a summary of the main concepts related to each paradigm:
-#
-# 1. Imperative: 
-#    * if else then
-#    * while, do until, for loops
-# 2. Functional
-#    * map, filter, reduce
-#    * curry, partial application 
-# 3. Object oriented 
-#
-# We are also going to implement these constructs from scratch
-#
-# * if else then: short circuit
-# * loops: goto or recursion
-# * map, filter, reduce: recursion
-# * curry, partial application
-#
-#
-# == Imperative
+# == Imperative constructs
 
 if true; then echo "it is true"; else echo "it is not true"; fi
 for i in 1 2 3 4 5; do echo $i; done
@@ -36,7 +8,7 @@ while true; do echo "this is an infinite loop"; done # <2>
 # (1) the `until true` loop can be thought of as a `while false` loop 
 # (2) the `while true` loop can be thought of as a `until false` loop 
 
-# === Implementing Imperative constructs: from scratch 
+# == Imperative constructs from scratch 
 
 true && echo "it is true" || echo "it is not true"              # <1>
 while_loop () { echo "yes" && true && while_loop; }             # <2>
@@ -46,7 +18,7 @@ while_loop () { echo "yes" && true && while_loop; }             # <2>
 # (2) while loop implemented with recursion
 # (3)   for loop implemented with recursion
 
-# == Functional
+# == Functional constructs
 
 #    * map, filter, reduce
 #    * curry, partial application 
@@ -77,32 +49,11 @@ echo One Two Three Four Five Six | # no
        grep --invert-match 'O|S'   # yep
 
 
-# === Implementing Functional constructs: from scratch 
+# == Functional constructs from scratch 
 
-# .if else then: short circuit
-
-true && echo "it is true" || echo "it is not true"
-
-# .loops: goto or recursion
-
-true && echo "it is true" || echo "it is not true"
 
 # == Object-Oriented
 
-#    * map, filter, reduce
-#    * curry, partial application 
 
-if true then echo "it is true"; else echo "it is not true";
-for i in 1 2 3 4 5; echo $i; done
-while true; do echo "this is an infinite loop"; done
-
-# === Implementing Object-Oriented constructs: from scratch 
-
-# .if else then: short circuit
-
-true && echo "it is true" || echo "it is not true"
-
-# .loops: goto or recursion
-
-true && echo "it is true" || echo "it is not true"
+# === Object-Oriented constructs from scratch 
 
