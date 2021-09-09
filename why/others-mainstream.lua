baa76fdc74c3fdd68c1c5b4586ea762f92ac0091
Bruno-366 <81762173+Bruno-366@users.noreply.github.com>
2021-09-09 11:42:53 +0200

    Update others-mainstream.lua

--- why/others-mainstream.lua
+++ why/others-mainstream.lua
@@ -11,11 +11,15 @@ while true do print("this is an infinite loop"); end  -- <2>
 -- == Imperative constructs from scratch 
 
 print(true and "it is true" or "it is not true")              -- <1>
-function while_loop () print("yes") and true and while_loop(); end            -- <2>
+function while_loop () print("yes"); return true and while_loop(); end            -- <2>
 function for_loop (i) i = i+1; print(i); return (i == 5) or for_loop(i); end  -- <3>
 for_loop(0)
+while_loop()
 
 -- (1) if else then, implemented with short circuit
+--     print in lua is evaluated as a falsey value,
+--     so the following would print both "it is true" and "it is not true":
+--     return true and print("it is true") or print("it is not true") 
 -- (2) while loop implemented with recursion
 -- (3)   for loop implemented with recursion
 
